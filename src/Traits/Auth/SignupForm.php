e173a980b71f20e64a2ac186302d546254a466d9
joy2fun <joy2fun@users.noreply.github.com>
2024-08-01 10:44:44 +0000

    Fix styling

--- src/Traits/Auth/SignupForm.php
+++ src/Traits/Auth/SignupForm.php
@@ -2,15 +2,15 @@
 
 namespace Joy2fun\FilamentExt\Traits\Auth;
 
-use Filament\Forms\Get;
-use Filament\Forms\Form;
-use Filament\Support\Enums\Alignment;
-use Filament\Forms\Components\Section;
-use Joy2fun\FilamentExt\Models\SmsCode;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Support\Enums\Alignment;
 use Illuminate\Validation\Rules\Password;
 use Joy2fun\FilamentExt\Forms\Components\SmsCodeInput;
+use Joy2fun\FilamentExt\Models\SmsCode;
 
 trait SignupForm
 {
@@ -32,7 +32,7 @@ trait SignupForm
                     TextInput::make('password2')->password()->label('确认密码')->same('password')
                         ->dehydrated(false)
                         ->validationMessages([
-                            'same' => '两次密码不一致'
+                            'same' => '两次密码不一致',
                         ])
                         ->required(),
                     TextInput::make('mobile')->label('手机号')
@@ -40,7 +40,7 @@ trait SignupForm
                     SmsCodeInput::make('code')->hiddenLabel()
                         ->rules([
                             'required',
-                            fn(Get $get): \Closure => function (string $attribute, $value, \Closure $fail) use ($get) {
+                            fn (Get $get): \Closure => function (string $attribute, $value, \Closure $fail) use ($get) {
                                 SmsCode::attempt($get('mobile') ?? '', $value) or $fail('验证码无效');
                             },
                         ]),
@@ -50,7 +50,7 @@ trait SignupForm
                         Action::make('已有账号登录')->link()->url('signin'),
                     ])
                     ->footerActionsAlignment(Alignment::Center)
-                    ->extraAttributes(['class' => 'min-w-96'])
+                    ->extraAttributes(['class' => 'min-w-96']),
             ]);
     }
 }
